1ffb0003108abcaffcc177f53afaae118082e069
harsh-vish14 <harshkv9322@gmail.com>
2021-05-25 17:49:28 +0530

    removed bugs

--- pages/posts/[postSlug].js
+++ pages/posts/[postSlug].js
@@ -1,3 +1,4 @@
+import { getPageFiles } from "next/dist/next-server/server/get-page-files";
 import PostContent from "../../components/post/post-details/post-content";
 import { getPostData } from "../../lib/post-util";
 
@@ -20,7 +21,7 @@ export const getStaticProps = async (context) => {
 };
 
 export const getStaticPaths = (context) => {
-  const postFilenames = getPostsFiles();
+  const postFilenames = getPageFiles();
 const slugs = postFilenames.map((fileName) => fileName.replace(/\.md$/, ""));
 
 return {
